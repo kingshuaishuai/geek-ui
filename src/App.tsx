240294bb7b4baf66da09c0a33de28ecda92d3cd7
kingshuaishuai <1244943563@qq.com>
2020-06-23 12:35:30 +0800

    button组件开发与测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import Button, { ButtonSize, ButtonType } from './components/Button/button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>this is h1</h1>
-        <h2>this is h2</h2>
-        <h3>this is h3</h3>
+      <Button size={ButtonSize.Large} onClick={() => console.log('a')}>Hello World</Button>
+      <Button>Hello World</Button>
+      <Button autoFocus size={ButtonSize.Small}>autoFocus</Button>
+      <Button className="custom" btnType={ButtonType.Primary}>custom</Button>
+      <Button btnType={ButtonType.Danger}>Hello World</Button>
+      <Button btnType={ButtonType.Success}>Hello World</Button>
+      <Button btnType={ButtonType.Warning}>Hello World</Button>
+      <Button disabled>Hello World</Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank">Hello World</Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>Hello World</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
